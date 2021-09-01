79e0e089738296302911c352a3f51860792558db
pHedi3 <federico.gualdi3@gmail.com>
2021-09-01 15:36:13 +0200

    fine es

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['nome' => 'Gino']);
+});
+Route::get('/info', function () {
+    return view('info', ['mynome' => 'Carlo']);
 });
